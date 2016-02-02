81fd6a8f2ad4e03037eb932006b20c0af3dd4167
cshih299 <cshih299@gmail.com>
2016-02-02 18:37:31 -0500

    added a tester comment

--- app/src/main/java/com/polka/pdm/MainActivity.java
+++ app/src/main/java/com/polka/pdm/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // comment thingy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
