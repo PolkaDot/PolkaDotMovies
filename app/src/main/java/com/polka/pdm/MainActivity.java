6dd4e23e5d2f94daa02b612aa7e825542980c626
alishakc <alishakc002@gmail.com>
2016-02-04 17:31:31 -0500

    HomeScreen

--- app/src/main/java/com/polka/pdm/MainActivity.java
+++ app/src/main/java/com/polka/pdm/MainActivity.java
@@ -3,7 +3,6 @@ package com.polka.pdm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mainScreen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
