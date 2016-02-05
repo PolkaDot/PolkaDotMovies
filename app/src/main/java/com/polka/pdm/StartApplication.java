b745b88d43ace37d9064ab69dd825f6d8eab6192
arsh786 <amzehra@gmail.com>
2016-02-05 17:11:37 -0500

    merged with pretty home screen. working program.

--- app/src/main/java/com/polka/pdm/StartApplication.java
+++ app/src/main/java/com/polka/pdm/StartApplication.java
@@ -14,6 +14,9 @@ public class StartApplication extends AppCompatActivity  {
 
     }
 
+    // if you press log out, it will take you to the main activity screen
+    // future reference, go to layout, xml add button and set its on click
+    // to the method in this case logoutClickListener
     public void logoutClickListener(View view) {
         Intent intent = new Intent(StartApplication.this, MainActivity.class);
         startActivity(intent);
