090557457c3a3acac1120abedc806291a18b0cf3
arsh786 <amzehra@gmail.com>
2016-02-05 13:27:24 -0500

    adding startapplication.java file

--- app/src/main/java/com/polka/pdm/StartApplication.java
+++ app/src/main/java/com/polka/pdm/StartApplication.java
@@ -1,11 +1,6 @@
 package com.polka.pdm;
 import android.content.Intent;
-import android.database.Cursor;
-import android.content.CursorLoader;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.widget.Button;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,31 +11,12 @@ public class StartApplication extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_application);
-//        Button logout = findViewById(R.id.logout);
-//        logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(StartApplication.this, MainActivity.class);
-//                startActivity(intent);
-//            }
-////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-////                        .setAction("Action", null).show();
-////            }
-//        });
+
     }
 
     public void logoutClickListener(View view) {
         Intent intent = new Intent(StartApplication.this, MainActivity.class);
         startActivity(intent);
     }
-//    Button logout;
-
-//    logout.setOnClickListener(new View.OnClickListener() {
-//
-//        @Override
-//        public void onClick(View view) {
-//
-//        }
-//    });
 
 }
