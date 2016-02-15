be42af8e36848f71ba4b93a3ab5f4e7800bea5cd
esingh <esingh7@gatech.edu>
2016-02-14 22:07:06 -0500

    veiw profile no longer editable

--- app/src/main/java/com/polka/pdm/ViewProfile.java
+++ app/src/main/java/com/polka/pdm/ViewProfile.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ViewProfile extends AppCompatActivity {
 
@@ -16,15 +17,33 @@ public class ViewProfile extends AppCompatActivity {
         setContentView(R.layout.activity_view_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        UserRepo repo = new UserRepo(this);
+        User user = new User("user1", "pass1", "firstName1", "lastName1", "email1");
+        repo.insert(user);
+        String name = repo.getUserByUsername("user1").toString();
+        TextView NameTextView = (TextView)findViewById(R.id.NameTextField);
+        NameTextView.setText(name);
+        TextView UserTextView = (TextView)findViewById(R.id.UserTextField);
+        UserTextView.setText(name);
+        TextView PassTextView = (TextView)findViewById(R.id.PassTextField);
+        PassTextView.setText(name);
+        TextView PhoneTextView = (TextView)findViewById(R.id.PhoneTextField);
+        PhoneTextView.setText(name);
+        TextView EmailTextView = (TextView)findViewById(R.id.EmailTextField);
+        EmailTextView.setText(name);
+        TextView MajorTextView = (TextView)findViewById(R.id.MajorTextField);
+        MajorTextView.setText(name);
+        TextView InterestTextView = (TextView)findViewById(R.id.InterestTextField);
+        InterestTextView.setText(name);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     public void onEditProfileClick(View view) {
