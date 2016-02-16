d2859611cae55d83ed233b9a843e6ee705b45483
Yamilex Avila-Stanley <yjas3@gatech.edu>
2016-02-15 23:22:36 -0500

    Edit text fields contain user information from database

--- app/src/main/java/com/polka/pdm/EditProfile.java
+++ app/src/main/java/com/polka/pdm/EditProfile.java
@@ -8,16 +8,15 @@ package com.polka.pdm;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.EditText;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class EditProfile extends AppCompatActivity {
+ public class EditProfile extends AppCompatActivity {
 
     //when this activity is created, it makes a toolbar and stuff
     @Override
@@ -27,20 +26,33 @@ public class EditProfile extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        UserRepo repo = new UserRepo(this);
-        User user = repo.getUserByUsername("user1");
+        User user;
+        // Grab saved data about user
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                user = null;
+            } else {
+                user = extras.getParcelable("user");
+            }
+        } else {
+            user = savedInstanceState.getParcelable("user");
+        }
+        Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show();
+
+
         EditText username = (EditText)findViewById(R.id.UserTextField);
         username.setText(user.username);
-//        EditText pass = (EditText)findViewById(R.id.passwordText);
-//        pass.setText(user.password);
-//        EditText name = (EditText)findViewById(R.id.nameText);
-//        name.setText(user.firstName);
-//        EditText email = (EditText)findViewById(R.id.emailText);
-//        email.setText(user.email);
-//        EditText phone = (EditText)findViewById(R.id.PhoneTextField);
-//        phone.setText(user.phone);
-//        EditText interests = (EditText)findViewById(R.id.InterestsLabel);
-//        interests.setText(user.interests);
+        EditText pass = (EditText)findViewById(R.id.PassTextField);
+        pass.setText(user.password);
+        EditText name = (EditText)findViewById(R.id.NameTextField);
+        name.setText(user.firstName);
+        EditText email = (EditText)findViewById(R.id.EmailTextField);
+        email.setText(user.email);
+        EditText phone = (EditText)findViewById(R.id.PhoneTextField);
+        phone.setText(user.phone);
+        EditText interests = (EditText)findViewById(R.id.InterestsLabel);
+        interests.setText(user.interests);
 
 
 
--- app/src/main/java/com/polka/pdm/ViewProfile.java
+++ app/src/main/java/com/polka/pdm/ViewProfile.java
@@ -9,17 +9,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ViewProfile extends AppCompatActivity {
-    private String original;
+    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        UserRepo repo = new UserRepo(this);
 
         // Grab saved data about user
-        User user;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if (extras == null) {
@@ -32,44 +31,24 @@ public class ViewProfile extends AppCompatActivity {
         }
         Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show();
 
-        // Get TextViews on viewprofile page
+        // Get TextViews on view profile page
         TextView nameTextView = (TextView)findViewById(R.id.NameTextField);
         // TextView editTextLastName; TODO: add this into view profile page
         TextView userTextView = (TextView)findViewById(R.id.UserTextField);
         TextView emailTextView = (TextView)findViewById(R.id.EmailTextField);
         TextView passTextView = (TextView)findViewById(R.id.PassTextField);
 
-//        // Display user information
+        // Put user information in TextView boxes
         nameTextView.setText(user.firstName);
         userTextView.setText(user.username);
         emailTextView.setText(user.email);
         passTextView.setText(user.password);
-
-//        User user = new User("user1", "pass1", "firstName1", "lastName1", "email1");
-//        repo.insert(user);
-//        String name = repo.getUserByUsername("user1").toString();
-//        TextView NameTextView = (TextView)findViewById(R.id.NameTextField);
-//        NameTextView.setText(name);
-//        TextView UserTextView = (TextView)findViewById(R.id.UserTextField);
-//        UserTextView.setText(name);
-//        TextView PassTextView = (TextView)findViewById(R.id.PassTextField);
-//        PassTextView.setText(name);
-//        TextView PhoneTextView = (TextView)findViewById(R.id.PhoneTextField);
-//        PhoneTextView.setText(name);
-//        TextView EmailTextView = (TextView)findViewById(R.id.EmailTextField);
-//        EmailTextView.setText(name);
-//        TextView MajorTextView = (TextView)findViewById(R.id.MajorTextField);
-//        MajorTextView.setText(name);
-//        TextView InterestTextView = (TextView)findViewById(R.id.InterestTextField);
-//        InterestTextView.setText(name);
-//
-//        original = user.username;
-
     }
 
     public void onEditProfileClick(View view) {
         Intent intent = new Intent(this, EditProfile.class);
-//        intent.putExtra("username",original);
+        // putExtra to store user information between views
+        intent.putExtra("user", user);
         startActivity(intent);
     }
 
