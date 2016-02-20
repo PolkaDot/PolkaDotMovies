9ca0db2de07098f4340dfc8ff10804d07fe6647f
Yamilex Avila-Stanley <yjas3@gatech.edu>
2016-02-20 01:24:39 -0500

    added Singleton class. Created Temporary Method in HomeApp that makes a JSONRequest -- will move later

--- app/src/main/java/com/polka/pdm/HomeApp.java
+++ app/src/main/java/com/polka/pdm/HomeApp.java
@@ -2,17 +2,24 @@ package com.polka.pdm;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class HomeApp extends AppCompatActivity {
 
     private User user;
+    private String response;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +82,36 @@ public class HomeApp extends AppCompatActivity {
         Intent intent = new Intent(this, ViewProfile.class);
         intent.putExtra("user", user);
         startActivity(intent);
+    }
+
+    /**
+     * Test Method to see if call to API works.
+     * TODO: Implement in actual search activity when Arsh is done.
+     * @param view
+     */
+    public void onSearchMovieClick(View view) {
+        String apiKey = "yedukp76ffytfuy24zsqk7f5";
+        String baseUrl = "http://api.rottentomatoes.com/api/public/v1.0/";
+        String searchUrl = "movies.json?apikey=" + apiKey + "&q=Jack&page_limit=1";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, baseUrl + searchUrl, (String)null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject resp) {
+                        //handle a valid response coming back.  Getting this string mainly for debug
+                        response = resp.toString();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        response = "JSon Request Failed!!";
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
     }
 
 }
