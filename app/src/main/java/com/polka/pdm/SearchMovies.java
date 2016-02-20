8d10f85e0a60a4c57cb201a3d2da4242090cb0bf
arsh786 <amzehra@gmail.com>
2016-02-20 15:36:24 -0500

    Added Search Bar

---
+++ app/src/main/java/com/polka/pdm/SearchMovies.java
@@ -0,0 +1,37 @@
+package com.polka.pdm;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class SearchMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_movies);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+    }
+
+    public void onSearchButtonPress(View view) {
+        Log.d("SearchMovies", "search button pressed");
+
+
+    }
+
+}
