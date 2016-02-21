aa41005b9bc27dc42f41f3e3045052ec51792386
arsh786 <amzehra@gmail.com>
2016-02-21 01:55:29 -0500

    Added Navigation Bar (and implented it with search movie, view profile, and home app)

--- app/src/main/java/com/polka/pdm/MainActivity.java
+++ app/src/main/java/com/polka/pdm/MainActivity.java
@@ -54,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
